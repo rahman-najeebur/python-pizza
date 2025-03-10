ef7e3c47dab32346a824fa4c6d512279ac3b6d50
rahman-najeebur <rahman.najeebur01@gmail.com>
2025-03-10 22:33:37 +0100

    initial commit of python pizza files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# python-pizza
+# Python Pizza Deliveries
+#### Author: Najeeb Ur Rahman
+#### Description:
+Python Pizza is an automatic pizza order program that's allow
+user to choose the order and calculate the total bill.
+
+#### Pyhon Pizza Menu:
+* Small pizza (S): $15
+* Medium pizza (M): $20
+* Large pizza (L): $25
+
+Extra filling
+* pepperoni for small pizza: +$2
+* pepperoni for medium or large pizza: +$3
+* extra cheese for any size pizza:  +$1
+
---
+++ main.py
@@ -0,0 +1,43 @@
+print("Welcome to Python Pizza Deliveries!!!")
+currency = "$"
+bill = 0
+
+# display menu
+def display_menu():
+    print(10*"#" + " Python Pizza Menu " + 2 * "#")
+    print(f"Small pizza (S): {currency}10")
+    print(f"Medium pizza (M):{currency}15")
+    print(f"Large pizza (L): {currency}20")
+    print(30*"#")
+
+# extra filling
+def extra_filling(size):
+    amount = 0
+    add_pepperoni = input("Do you want pepperoni? Type Y if yes, N if no: ").lower()
+    if add_pepperoni == "y" and size == "s":
+        amount = 2
+    if add_pepperoni == "y" and (size == "m" or size == "l"):
+        amount = 3
+    extra_cheese = input("Do you want extra cheese? Type Y if yes, N if no: ").lower()
+    if extra_cheese == "y":
+        amount += 1
+    return amount
+
+# calling the display_menu() function
+display_menu()
+
+pizza_choice = input("please enter your pizza choice: ").lower()
+if pizza_choice == "s":
+    bill = 10
+    bill += extra_filling(pizza_choice)
+elif pizza_choice == "m":
+    bill = 15
+    bill += extra_filling(pizza_choice)
+elif pizza_choice == "l":
+    bill = 20
+    bill += extra_filling(pizza_choice)
+else:
+    print("Please select S for Small, M for Medium and L for Large Pizza.")
+
+print(f"Your Total bill is {currency}{bill}")
+
